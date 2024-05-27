2aa8baa2897004eb6dcbec47ad64dbd726ef7226
Kevin Sylvestre <kevin@ksylvest.com>
2024-05-27 14:05:57 -0700

    Ensure prettier and eslint do not conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintconfigprettier from "eslint-config-prettier";
 
-export default [...tseslint.configs.recommended];
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintconfigprettier,
+];
